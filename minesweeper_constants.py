80b395a7666a9b97182f7bf01a95a36b40ebe09f
Bryan Yu <70780073+bryanyu1@users.noreply.github.com>
2020-09-05 11:29:20 -0700

    Create minesweeper_constants.py

---
+++ minesweeper_constants.py
@@ -0,0 +1,11 @@
+# Constants
+grid3x3 = [[True ,False,False],
+           [False,False,False],
+           [False,False,True]]
+
+board3x3 = [[' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']]
+    
+## Testing Purposes
+# play_game(grid3x3,board3x3)
